dd2b36d71ea730d1c3b1f118a049ab3ce28bd03a
denebtech <techdeneb@gmail.com>
2019-02-09 10:58:25 -0500

    Add card

--- components/Main.js
+++ components/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {Button} from 'react-native-elements';
+import {Button, Card} from 'react-native-elements';
 import {Actions} from 'react-native-router-flux';
 import Questions from '../json_files/questions';
 import Randomizer from 'react-randomizer';
@@ -25,14 +25,31 @@ export default class Main extends React.Component {
 
   start = () => {
     const currentQuestion = this.state.remainingQuestions[0];
-    console.log(typeof question, 'one question')
-    this.setState({currentQuestion})
+    const newRemaining = this.state.remainingQuestions.slice(1);
+    this.setState({currentQuestion});
+    this.setState({remainingQuestions: newRemaining});
     this.setState({showStartButton: false});
   }
 
+  randomQuestion = () => {
+    const newPrevious = this.state.previousQuestions.concat([this.state.currentQuestion]);
+    this.setState({previousQuestions: newPrevious});
+    if(this.state.remainingQuestions.length === 1) {
+      const currentQuestion = this.state.remainingQuestions[0];
+      const randomized = Randomizer.randomizeArray(this.state.questions);
+      this.setState({remainingQuestions: randomized});
+      this.setState({currentQuestion});
+    } else {
+      const currentQuestion = this.state.remainingQuestions[0];
+      const newRemaining = this.state.remainingQuestions.slice(1);
+      this.setState({remainingQuestions: newRemaining});
+      this.setState({currentQuestion});
+    }
+  }
+
   render() {
-    console.log(this.state.questions, 'questions')
     console.log(this.state.remainingQuestions, 'remaining questions')
+    console.log(this.state.previousQuestions, 'previous questions')
     return (
       <View style={styles.container}>
         {
@@ -43,26 +60,32 @@ export default class Main extends React.Component {
               backgroundColor="green"
               icon={{name: 'forward'}}
               title='Start'
-              onPress={() => this.start()}
+              onPress={this.start}
             />
           </View>
           :
           <View style={styles.container}>
-            <Text>{this.state.currentQuestion.question}</Text>
+            <Card
+              title='Question'
+              image={require('../assets/question_mark.png')}>
+              <Text style={{marginBottom: 10}}>
+              {this.state.currentQuestion.question}
+              </Text>
+            </Card>
             <Button
               raised
               backgroundColor="green"
               icon={{name: 'autorenew'}}
-              title='Next Question'
-              onPress={() => Actions.main()}
+              title='Next Random Question'
+              onPress={this.randomQuestion}
             />
-            <Button
+            {this.state.previousQuestions.length ? <Button
               raised
               backgroundColor="red"
               icon={{name: 'replay'}}
               title='Previous Question'
               onPress={() => console.log('previous question pressed')}
-            />
+            /> : null}
             <Button
               raised
               backgroundColor="blue"
